f7354b8c4b1fa4c7d8c808a48531a6c8ab6a08cd
Joel Birch <joeldbirch@gmail.com>
2019-11-19 12:05:52 +1100

    Fix thank you page.

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet'
 import { callOut } from '../styles/helpers'
 
 import Main from '../components/TheMain'
-import Image from '../components/image'
 import Layout from '../components/layout'
 
 import TheFooter from '../components/TheFooter'
@@ -22,11 +21,11 @@ const ThankYouPage = () => (
     />
 
     <Main>
-      <Image />
       <div>
         <h1 className={callOut}>
           Thank you for your enquiry, we will be in contact&#160;soon!
         </h1>
+        <a href="/">Go back</a>
       </div>
     </Main>
 
