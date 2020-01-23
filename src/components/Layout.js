e7d12f89acb3d696d2c0af9d66de9b9f585bb88d
Joel Birch <joeldbirch@gmail.com>
2020-01-23 14:55:24 +1100

    Extract files into own package.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,7 +17,7 @@ import TheHeader from './TheHeader'
 import TheFooter from './TheFooter'
 import { isIos, isSafari } from '../utilities/helpers'
 import { uiFontSize } from '../styles/helpers'
-import '../styles/index.scss'
+import '@joeldbirch/fortis-styles'
 
 const Layout = ({
   children,
