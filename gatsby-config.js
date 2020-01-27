bca404befa4098c014aa802e428db0089687cdf1
Joel Birch <joeldbirch@gmail.com>
2020-01-27 13:31:39 +1100

    Exclude css reset.

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,7 +106,7 @@ module.exports = {
         debug: true,
         printRejected: true, // Print removed selectors and processed file names
         content: ['public/*.html', 'public/**/*.html', 'src/**/*.js','src/*.js', 'src/images/*.svg', 'src/images/**/*.svg', 'src/fortis-styles/**/!(*.d).{js,jsx}'],
-        ignore: [`/src/components/FormModal/_index.scss`, `/src/components/TheWrap/style.module.scss`, `flickity`],
+        ignore: [`/src/components/FormModal/_index.scss`, `/src/components/TheWrap/style.module.scss`, `flickity`, `/src/fortis-styles/base/_custom-reset.scss`],
         whitelist: wordpressSelectors,
         whitelistPatterns: wordpressPatterns,
         extractors: [
