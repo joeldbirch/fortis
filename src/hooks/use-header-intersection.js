674bb607447090e67e53f9c6e39fe517370ba3f9
Joel Birch <joeldbirch@gmail.com>
2020-01-29 15:27:27 +1100

    IntersectionObserver now works.

--- src/hooks/use-header-intersection.js
+++ src/hooks/use-header-intersection.js
@@ -7,27 +7,16 @@ const useHeaderIntersection = function() {
   const init = function() {
     const sections = document.querySelectorAll(`section, .isSection`)
     const intersectionConfig = {
-      rootMargin: `0px`,
-      threshold: [0, .1], // TODO: tweak intersection threshold
+      // 90% offset bottom to make the test area similar to where header is
+      rootMargin: `0px 0px -90% 0px`,
+      threshold: 0,
     }
-    const header = document.getElementById(`SiteHeader`)
-
-    const observer = new IntersectionObserver(function (entries, self) {
+    const observer = new IntersectionObserver(function (entries) {
       entries.forEach(entry => {
         const { target } = entry
         if (entry.isIntersecting) {
-          const {top, bottom} = target.getBoundingClientRect()
-          const behindHeader = top < 16 && bottom > header.offsetHeight
           const shouldReverse = target.dataset.reverse === "true"
-          // console.log({
-          //   shouldReverse,
-          //   behindHeader,
-          //   target,
-          //   reverse: target.dataset.reverse,
-          //   sectionTop: top,
-          //   sectionBottom: bottom,
-          // });
-          if (behindHeader) setHeaderReversed(shouldReverse)
+          setHeaderReversed(shouldReverse)
         }
       })
     }, intersectionConfig)
