209c155d50bf2197475990eb87cce1fc755b2395
Joel Birch <joeldbirch@gmail.com>
2020-01-29 18:21:55 +1100

    Fix some padding.

--- src/layouts/project/TextLeftTwoImages/TextLeftTwoImages.js
+++ src/layouts/project/TextLeftTwoImages/TextLeftTwoImages.js
@@ -7,6 +7,7 @@ const styles = {
   root: `
     padding-horizontal:columns-0-1/2
     @mq-desk--padding-horizontal:columns-1-1/2
+    @mq-desk--padding-vertical:columns-0-1/2
     padding-vertical:800
     position:relative
     width:100
