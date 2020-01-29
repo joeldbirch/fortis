f47b25eb60306223895f5e9911f816b43574d14d
Joel Birch <joeldbirch@gmail.com>
2020-01-29 18:15:05 +1100

    Give default blank className prop.

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TheHeader = ({children, className, ...props}) => {
+const TheHeader = ({children, className=``, ...props}) => {
   return (
     <header
       className={`
