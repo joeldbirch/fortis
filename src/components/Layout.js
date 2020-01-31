60a1019d66720b8eb84c78672e8998cc9b5289bc
Joel Birch <joeldbirch@gmail.com>
2020-01-31 17:41:13 +1100

    Remove guides.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -76,7 +76,6 @@ const Layout = ({
           font-size:300
           @mq-lap--font-size:400
           min-height:100vh-fixed
-          grid-guide
         `} lang="en" />
 
         <meta name="robots" content="noindex" />
