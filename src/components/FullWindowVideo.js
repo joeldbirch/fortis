734bc27b503d32e19580877985101944f2ffeec8
Joel Birch <joeldbirch@gmail.com>
2020-02-03 13:17:30 +1100

    Turn on autoplay video.

--- src/components/FullWindowVideo.js
+++ src/components/FullWindowVideo.js
@@ -38,7 +38,7 @@ const FullWindowVideo = ({vimeoId, className=``, windowHeight=false, height=`56.
         }}
         frameBorder="0"
         height="1920"
-        src={`https://player.vimeo.com/video/${vimeoId}?background=1&playsinline=1&autoplay=0&loop=1&autopause=0&muted=1`}
+        src={`https://player.vimeo.com/video/${vimeoId}?background=1&playsinline=1&autoplay=1&loop=1&autopause=0&muted=1`}
         title="heroVideo"
         width="1080"
         loading="lazy"
