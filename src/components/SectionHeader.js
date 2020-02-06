818d3bece003cfc4719c61c15e086f8beb179ac5
Joel Birch <joeldbirch@gmail.com>
2020-02-07 09:55:16 +1100

    Center section headings on mobile.

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -17,6 +17,7 @@ export default ({className=``, absolute=`true`, children}) => {
       <div
         className={`
           padding-horizontal:columns-0-1/2
+          @mq-max-palm--text-align:center
           @mq-palm--padding-left:columns-3-1/2
           @mq-palm--padding-right:columns-1-1/2
           width:100
