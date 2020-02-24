2692288cd0bf72aae8e2e190fd4dc7446bb0a701
Joel Birch <joeldbirch@gmail.com>
2020-02-24 16:59:53 +1100

    Refactor FluidImage slightly.

--- src/components/FluidImage.js
+++ src/components/FluidImage.js
@@ -14,27 +14,28 @@ const FluidImage = ({ image, withFallback = true, className=``, artDirection=`im
   /**
    * Return fallback image, if no image is given.
    */
-  if (!image) {
-    return withFallback ? <div className={`${className}`}><img src={data.fallBackImage.publicURL} alt="" className={`width:100 height:100`} {...props} /></div> : null
+  if (!image && withFallback) {
+    return (
+      <div className={`${className}`}>
+        <img src={data.fallBackImage.publicURL} alt="" className={`width:100 height:100`} {...props} />
+      </div>
+    )
   }
 
   if (image[artDirection]) {
-
     const { fluid } = image[artDirection].childImageSharp
-    const newFluid = !sizes
-      ? fluid
-      : {
-        ...fluid,
-        sizes,
-      }
-
-    return <GatsbyImage
-      fluid={newFluid}
-      alt={image.altText || ``}
-      className={className}
-      durationFadeIn={1000}
-      {...props}
-    />
+    return (
+      <GatsbyImage
+        fluid={{
+          ...fluid,
+          sizes: sizes || fluid.sizes,
+        }}
+        alt={image.altText || ``}
+        className={className}
+        durationFadeIn={1000}
+        {...props}
+      />
+    )
   }
 
   return <img src={image.sourceUrl} alt={image.altText} className={`${className} width:100`} {...props} />
