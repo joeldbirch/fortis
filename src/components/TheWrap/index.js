e0dc742bdf879b76151f4c46a4ed306da7e6aad4
Joel Birch <joeldbirch@gmail.com>
2020-02-24 18:30:45 +1100

    Extract freescroll watcher to custom hook.

--- src/components/TheWrap/index.js
+++ src/components/TheWrap/index.js
@@ -1,32 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
+import useFreeScroll from 'hooks/use-free-scroll'
 import styles from './style.module.scss'
 
 
 export default ({className=``, children, ...props }) => {
 
-  const [freeScroll, setFreeScroll] = useState(false)
-
-  const initFreeScroll = function() {
-    const sections = document.querySelectorAll(`.js-free-scroll`)
-
-    const observer = new IntersectionObserver(function (entries) {
-      entries.forEach(entry => {
-        // console.log({
-        //   y: entry.boundingClientRect.y,
-        //   root: entry.rootBounds.y,
-        // });
-
-        if (entry.boundingClientRect.y < 0) {
-          setFreeScroll(true)
-        } else {
-          setFreeScroll(false)
-        }
-      })
-    })
-
-    sections.forEach(section => observer.observe(section))
-  }
-
+  const [freeScroll, initFreeScroll] = useFreeScroll()
   useEffect(initFreeScroll, [])
 
   return (
---
+++ src/hooks/use-free-scroll.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+
+export default () => {
+  const [freeScroll, setFreeScroll] = useState(false)
+
+  const init = function() {
+    const sections = document.querySelectorAll(`.js-free-scroll`)
+
+    const observer = new IntersectionObserver(function (entries) {
+      entries.forEach(entry => {
+        // console.log({
+        //   y: entry.boundingClientRect.y,
+        //   root: entry.rootBounds.y,
+        // });
+
+        if (entry.boundingClientRect.y < 0) {
+          setFreeScroll(true)
+        } else {
+          setFreeScroll(false)
+        }
+      })
+    })
+
+    sections.forEach(section => observer.observe(section))
+  }
+
+  return [freeScroll, init]
+}
