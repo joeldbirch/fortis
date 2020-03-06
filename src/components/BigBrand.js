157c81c818e92044fe46c3af551f179d849ab2fb
Joel Birch <joeldbirch@gmail.com>
2020-03-06 12:58:18 +1100

    Resort classes.

--- src/components/BigBrand.js
+++ src/components/BigBrand.js
@@ -13,12 +13,13 @@ const styles = {
     @mq-bigdesk--margin-top:400
     @mq-desk--padding-left:800
     @mq-desk--transform:var-2
-    @mq-max-palm--margin-horizontal:auto
-    @mq-palm--padding-bottom:400
-    @mq-widepalm--padding-bottom:600
     @mq-lap--padding-bottom:columns-0-1/2
+    @mq-max-palm--margin-horizontal:auto
+    @mq-max-palm--padding-top:400
+    @mq-palm--padding-bottom:400
     @mq-palm--padding-left:400
     @mq-widepalm--font-size:500
+    @mq-widepalm--padding-bottom:600
     @mq-widepalm--padding-left:800
     font-family:cursive
     font-size:500
@@ -27,7 +28,6 @@ const styles = {
     letter-spacing:0
     line-height:200
     padding-bottom:100
-    @mq-max-palm--padding-top:400
     position:relative
     transform:var-1
     white-space:pre
