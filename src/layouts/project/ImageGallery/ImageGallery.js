40c237868bdc660b45ffa933e75560671a8ac530
Joel Birch <joeldbirch@gmail.com>
2020-03-09 15:48:19 +1100

    Make image gallery content width.

--- src/layouts/project/ImageGallery/ImageGallery.js
+++ src/layouts/project/ImageGallery/ImageGallery.js
@@ -12,48 +12,57 @@ const ImageGallery = ({imageGallery}) => {
       className={`
         js-contrast-ignore
         margin-bottom:-columns-0-1/2
-        overflow:hidden
-        padding-bottom:columns-1
-        padding-top:columns-0-1/2
+        margin-horizontal:auto
+        max-width:container
+        padding-horizontal:columns-0-1/2
         position:relative
         scroll-snap-align:center
         width:100
       `}
     >
-      <Flickity
-        options={{arrowShape: path}}
-        static={true}
+      <div
+        className={`
+          overflow:hidden
+          padding-bottom:columns-1
+          padding-top:columns-0-1/2
+        `}
       >
-        { imageGallery.map((image, index) => (
-          <div key={index} className={`
-            js-contrast
-            js-contrast--reverse
-            position:relative
-            width:100
-          `}>
-            <FluidImage
-              image={image}
-              artDirection={`imageFileHero`}
-              className={`
-                height:100
-                width:100
-              `}
-            />
-            <div
-              className={`
-                handwritten
-                padding-top:columns-0-1/4
-                pos-right:0
-                pos-top:100
-                position:absolute
-                width:33
-              `}
-              dangerouslySetInnerHTML={{__html: image.caption}}
-            />
-          </div>
-        ))}
-      </Flickity>
-
+        <Flickity
+          options={{arrowShape: path}}
+          static={true}
+          className={`
+          `}
+        >
+          { imageGallery.map((image, index) => (
+            <div key={index} className={`
+              js-contrast
+              js-contrast--reverse
+              position:relative
+              width:100
+            `}>
+              <FluidImage
+                image={image}
+                artDirection={`imageFileHero`}
+                className={`
+                  height:100
+                  width:100
+                `}
+              />
+              <div
+                className={`
+                  handwritten
+                  padding-top:columns-0-1/4
+                  pos-right:0
+                  pos-top:100
+                  position:absolute
+                  width:33
+                `}
+                dangerouslySetInnerHTML={{__html: image.caption}}
+              />
+            </div>
+          ))}
+        </Flickity>
+      </div>
     </section>
   )
 }
