aba483f8212978310f051f07f1c272a40d19acad
Joel Birch <joeldbirch@gmail.com>
2020-03-09 15:55:49 +1100

    Fix  attribute for image gallery images.

--- src/layouts/project/ImageGallery/ImageGallery.data.js
+++ src/layouts/project/ImageGallery/ImageGallery.data.js
@@ -12,7 +12,7 @@ module.exports = () => {
               maxWidth: 1615,
               maxHeight: 970,
               cropFocus: CENTER,
-              sizes: "(max-aspect-ratio: 16/10) calc(100vh * 1.6), 100vw",
+              sizes: "(min-width: 1400px) 1250px, 100vw",
               srcSetBreakpoints: [ 800, 1250, 1600, 1920 ]) {
                 ...GatsbyImageSharpFluid_withWebp_noBase64
             }
