0d225bf35dfb89635d73ae575e40d64114711895
Joel Birch <joeldbirch@gmail.com>
2020-03-12 14:30:00 +1100

    Delete map location data.

--- src/layouts/page/Location/Location.data.js
+++ src/layouts/page/Location/Location.data.js
@@ -2,14 +2,6 @@ module.exports = () => {
   return `
     ... on WPGraphQL_Page_Pagebuilder_Layouts_GoogleMap {
       fieldGroupName
-      location {
-        city
-        streetAddress
-        postCode
-        zoom
-        longitude
-        latitude
-      }
       nearby {
         amenity
       }
--- src/layouts/project/ProjectLocation/ProjectLocation.data.js
+++ src/layouts/project/ProjectLocation/ProjectLocation.data.js
@@ -2,14 +2,6 @@ module.exports = () => {
   return `
     ... on WPGraphQL_Project_Projectbuilder_Layouts_GoogleMap {
       fieldGroupName
-      location {
-        city
-        streetAddress
-        postCode
-        zoom
-        longitude
-        latitude
-      }
       nearby {
         amenity
       }
