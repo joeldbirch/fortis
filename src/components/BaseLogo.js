2bbc952529b4dd435bf2228b0aa8f2b47f392854
Joel Birch <joeldbirch@gmail.com>
2020-03-16 10:01:30 +1100

    Use false rather than null for imageFile default.

--- src/components/BaseLogo.js
+++ src/components/BaseLogo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import FluidImage from './FluidImage'
 
-const BaseLogo = ({ logo={imageFile:null}, alt=``, fallbackClasses, ...props }) => {
+const BaseLogo = ({ logo={imageFile:false}, alt=``, fallbackClasses, ...props }) => {
   const { imageFile } = logo
 
   switch (true) {
