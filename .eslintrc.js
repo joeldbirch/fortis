6295e849c81dc1fc8230118666233538b5936ec2
Joel Birch <joeldbirch@gmail.com>
2020-04-26 09:53:42 +1000

    Setup eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,19 @@ module.exports = {
     __PATH_PREFIX__: true,
   },
   extends: `react-app`,
+  parser: "babel-eslint",
+  rules: {
+    strict: 0,
+    semi: ["error", "never"],
+    'no-console': process.env.NODE_ENV === `production` ? `error` : `off`,
+    'no-debugger': process.env.NODE_ENV === `production` ? `error` : `off`,
+    quotes: [`error`, `backtick`],
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "always-multiline",
+      "exports": "always-multiline",
+      "functions": "never"
+  }]
+  },
 }
